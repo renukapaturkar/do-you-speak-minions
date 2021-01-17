92aecb77c29d22bce0a6d0651da6aa1ac4ed6cbd
Renuka Paturkar <67357703+renukapaturkar@users.noreply.github.com>
2021-01-17 14:44:53 +0530

    update: app.js

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output-div");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" +text;
+}
+
+function errorHandler(error){
+    console.log("error occured! ", error);
+    alert("something wrong with server, try again after some time");
+}
+
+
+
 function clickHandler(){
-    outputDiv.innerText = "joisdehmdw,e " + textInput.value;
+    var inputText = textInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => { 
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 
 };
 
